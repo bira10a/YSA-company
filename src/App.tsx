a15ce8932f6eaef40b122362bd84ae316da84a12
bira10a <94951002+bira10a@users.noreply.github.com>
2025-11-27 17:14:38 +0300

    added Header.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { Header } from "./components/header/Header";
+
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <>
+      <Header />
+    </>
   )
 }
 export default App;
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,28 @@
+const Header = () => {
+     return (
+         <header className="header">
+            <div className="header__container">
+                <a href="#">
+                    <img width="200" height="150" alt="логотип" />
+                </a>
+
+                <nav className="header__nav">
+                    <ul>
+                        <li>
+                            <a href="\">О нас</a>
+                        </li>
+
+                        <li>
+                            <a href="\">О Вас</a>
+                        </li>
+
+                        <li>
+                            <a href="\">За северный кавказ</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+         </header>
+     )
+}
+export {Header};
