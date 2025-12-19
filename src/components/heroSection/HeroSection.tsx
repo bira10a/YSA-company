0df774a9d75dacc6726c652eb3c0732002d7bc68
bira10a <94951002+bira10a@users.noreply.github.com>
2025-12-19 16:52:10 +0300

    work ysa

--- src/components/heroSection/HeroSection.tsx
+++ src/components/heroSection/HeroSection.tsx
@@ -1,10 +1,10 @@
-import './_HeroSection.scss';
+import './_heroSection.scss';
 
 const HeroSection = () => {
      return (
          <section className="HeroSection">
             <div className="HeroSection__container">
-             <h2> <div>ООО "Югспецавтоматика"</div> Строительство, техперевооружение, реконструкция, пусконаладка на объектах энергетического и нефтегазового комплекса</h2>
+             <h2 className='HeroSection__title title'> <div>ООО "Югспецавтоматика"</div> Строительство, техперевооружение, реконструкция, пусконаладка на объектах энергетического и нефтегазового комплекса</h2>
             </div>
          </section>
      )
