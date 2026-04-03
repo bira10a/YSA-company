11cd7943102da3c4e8fb93eb29c4f3be1e5724d9
Nikita Rod() <bira10a@yandex.ru>
2026-04-03 22:29:42 +0300

    adding styles to the project

--- src/app/Layout.tsx
+++ src/app/Layout.tsx
@@ -3,13 +3,13 @@ import { Outlet } from "react-router";
 
 const Layout = () => {
   return (
-    <>
+    <div className="app">
       <Header />
       <main>
         <Outlet />
       </main>
       <footer>2026</footer>
-    </>
+    </div>
   ) 
 } 
 export {Layout};
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './app/App'
 import { BrowserRouter } from 'react-router'
-// import '../../style/style.scss';
+import './app/style/style.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
