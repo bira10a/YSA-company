485462974df23fa91b86c073b50605ee890026ca
bira10a <94951002+bira10a@users.noreply.github.com>
2026-04-06 16:58:49 +0300

    added hook useTheme v1/2

--- src/app/theme/useTheme.ts
+++ src/app/theme/useTheme.ts
@@ -1,7 +1,23 @@
-interface useThemeProps {
+import { useContext } from "react";
+import { LOCAL_STORAGE_THEME_KEY } from "./ThemeContext";
+
+interface useThemeResult {
   toggleTheme: () => void;
+  theme: Theme;
 }
 
-export function useTheme() {
+export function useTheme(): useThemeResult {
+  const {theme, setTheme} = useContext(ThemeContext);
 
+
+  const toggleTheme = () => {
+    const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
+    setTheme(newTheme);
+    localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
+  }
+
+  return {
+    theme, 
+    toggleTheme
+  }
 }
