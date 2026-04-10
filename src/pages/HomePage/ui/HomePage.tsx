ce99b7c8493840bf1d5e3d854b8185c90c4198d8
Nikita Rod() <bira10a@yandex.ru>
2026-04-10 20:42:01 +0300

    added markup HomePage Section

--- src/pages/HomePage/ui/HomePage.tsx
+++ src/pages/HomePage/ui/HomePage.tsx
@@ -1,10 +1,19 @@
 import './_HomePage.scss';
 
+import videoBackground from "./img/videoBackground.webm";
+
 const HomePage = () => {
      return (
          <section className="HomePage">
+                <div className="HomePage__background">
+                    <video autoPlay muted playsInline loop src={videoBackground} />
+                </div>
             <div className="HomePage__container">
-            <h2 className='HeroSection__title title'> HomePage</h2>
+
+                <h2 className='HomePage__title title'>
+                    <div>ООО «Югспецавтоматика»</div>
+                     Строительство, техперевооружение, реконструкция, пусконаладка на объектах энергетического и нефтегазового комплекса
+                </h2>
             </div>
          </section>
      )
