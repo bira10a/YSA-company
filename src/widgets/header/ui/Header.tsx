c67b6d338794731754743baa6e92141322c86b37
Nikita Rod() <bira10a@yandex.ru>
2026-04-16 23:58:31 +0300

    added burger button of the component Header

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,35 +1,47 @@
 import Logo from './img/logo.svg';
-import './_header.scss';
 
 import { NavLink } from 'react-router';
+import { useState } from 'react';
 
 const Header = () => {
+    const [burger, setBurger] = useState(false);
+
+    const BurgerActive = () => {
+        setBurger(burger => !burger);
+        
+    }
+
      return (
          <header className="header">
             <div className="header__container">
-                <a className='header__logo' href="#">
+                <NavLink className="header__logo header__link" to="/">
                     <img src={Logo} width="150" height="100" alt="логотип" />
-                </a>
+                </NavLink>
 
                 <nav className="header__nav">
                     <ul>
                         <li>
-                            <NavLink to="/">О компании</NavLink>
+                            <NavLink className="header__link" to="/">О компании</NavLink>
                         </li>
 
                         <li>
-                            <NavLink to="/HomePage">HomePage</NavLink>
+                            <NavLink className="header__link" to="/HomePage">HomePage</NavLink>
                         </li>
 
                         <li>
-                            <NavLink to="/VacancyPage">VacancyPage</NavLink>
+                            <NavLink className="header__link" to="/VacancyPage">VacancyPage</NavLink>
                         </li>
 
                         <li>
-                            <NavLink to="/heroSection213123213">NotFoundPage</NavLink>
+                            <NavLink className="header__link" to="/heroSection213123213">NotFoundPage</NavLink>
                         </li>
                     </ul>
                 </nav>
+
+                 <button className={`header__burger burger ${burger ? "burger--active" : ""}`} type="button" onClick={BurgerActive}>
+                     <span className="burger__line"></span>
+                     <span className="sr-only">Открыть меню</span>
+                 </button>
             </div>
          </header>
      )
